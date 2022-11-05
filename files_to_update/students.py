99a4f2960b0288ed233e9461faaa2f8a7eccbb17
Andreea Avramescu <33720256+andreea-avramescu@users.noreply.github.com>
2022-11-05 20:30:34 +0000

    Update students.py

--- files_to_update/students.py
+++ files_to_update/students.py
@@ -1,15 +1,17 @@
 students = [
-    "Impure Regressors", # Sebastian & Aslam & Komal
-    "Daniel_A",
-    "Daniel_D",
-    "Huy",
-    "Icaro",
-    "Ilkay",
-    "Kayla",
-    "Nabil",
-    "Orjada",
-    "Peter",
-    "Sherwan",
-    "ToSi", # Tobias & Simon
-    "wbs_bot"
+    "Andreea",
+    "Christoph",
+    "Hajra",
+    "Henrik",
+    "Marcelo",
+    "Masoumeh",
+    "Osazee",
+    "Mathew",
+    "Prachee",
+    "Ruben",
+    "Shiwali",
+    "Swathy",
+    "Trang",
+    "Yeeun",
+    "Yusuf",
 ]
